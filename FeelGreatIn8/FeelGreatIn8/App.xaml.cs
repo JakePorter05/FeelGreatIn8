26f40a0b45b1af602c1369e56b3b2bcfe891ea36
Jake Porter <jakeporter05@gmail.com>
2021-01-04 09:16:29 -0700

    Original commit

---
+++ FeelGreatIn8/FeelGreatIn8/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FeelGreatIn8.Services;
+using FeelGreatIn8.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FeelGreatIn8
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FeelGreatIn8/FeelGreatIn8/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FeelGreatIn8.Models;
+using FeelGreatIn8.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FeelGreatIn8.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
